329f303eb578a6208b95ca14de25ed1e49eb6ed2
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-14 11:01:35 +0800

    Add to Cart Feature

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 import { Row, Col } from 'antd';
 import ProductImage from './Sections/ProductImage';
 import ProductInfo from './Sections/ProductInfo';
-// import { addToCart } from '../../../_actions/user_actions';
+import { addToCart } from '../../../_actions/user_actions';
 import { useDispatch } from 'react-redux';
 function DetailProductPage(props) {
     const dispatch = useDispatch();
@@ -19,7 +19,7 @@ function DetailProductPage(props) {
     }, [])
 
     const addToCartHandler = (productId) => {
-    //    dispatch(addToCart(productId))
+        dispatch(addToCart(productId))
     }
 
     return (
