ce29558e18b1ce6c433ccec84991491d1b91a802
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-14 12:08:35 +0800

    Cart Page(1)

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -4,7 +4,10 @@ import {
     REGISTER_USER,
     AUTH_USER,
     LOGOUT_USER,
-    ADD_TO_CART_USER
+    ADD_TO_CART_USER,
+    GET_CART_ITEMS_USER,
+    REMOVE_CART_ITEM_USER,
+    ON_SUCCESS_BUY_USER
 } from './types';
 import { USER_SERVER } from '../components/Config.js';
 
@@ -55,6 +58,65 @@ export function addToCart(_id) {
 
     return {
         type: ADD_TO_CART_USER,
+        payload: request
+    }
+}
+
+
+export function getCartItems(cartItems, userCart) {
+    const request = axios.get(`/api/product/products_by_id?id=${cartItems}&type=array`)
+        .then(response => {
+
+
+            //Make CartDetail inside Redux Store 
+            // We need to add quantity data to Product Information that come from Product Collection. 
+
+            userCart.forEach(cartItem => {
+                response.data.forEach((productDetail, i) => {
+                    if (cartItem.id === productDetail._id) {
+                        response.data[i].quantity = cartItem.quantity;
+                    }
+                })
+            })
+
+            return response.data;
+        });
+
+    return {
+        type: GET_CART_ITEMS_USER,
+        payload: request
+    }
+}
+
+
+export function removeCartItem(id) {
+    const request = axios.get(`/api/users/removeFromCart?_id=${id}`)
+        .then(response => {
+
+            response.data.cart.forEach(item => {
+                response.data.cartDetail.forEach((k, i) => {
+                    if (item.id === k._id) {
+                        response.data.cartDetail[i].quantity = item.quantity
+                    }
+                })
+            })
+            return response.data;
+        });
+
+    return {
+        type: REMOVE_CART_ITEM_USER,
+        payload: request
+    }
+}
+
+
+export function onSuccessBuy(data) {
+
+    const request = axios.post(`${USER_SERVER}/successBuy`, data)
+        .then(response => response.data);
+
+    return {
+        type: ON_SUCCESS_BUY_USER,
         payload: request
     }
 }
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
 import UploadProductPage from './views/UploadProductPage/UploadProductPage';
 import DetailProductPage from './views/DetailProductPage/DetailProductPage';
+import CartPage from './views/CartPage/CartPage';
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -23,7 +24,9 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
           <Route exact path="/product/:productId" component={Auth(DetailProductPage, null)} />
+          <Route exact path="/user/cart" component={Auth(CartPage, true)} />
         </Switch>
       </div>
       <Footer />
